5fb1a8aea9f1f226abc5613076ee07fabd28541e
ShahSau <shahriarksaurov@gmail.com>
2021-06-29 11:45:12 +0300

    navbar

---
+++ froentend/src/App.js
@@ -0,0 +1,22 @@
+import { Container } from 'react-bootstrap'
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>My app</h1>
+        </Container>
+      </main>
+     
+
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
