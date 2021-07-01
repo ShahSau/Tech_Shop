4094020eeea88b4c5e2c5d7386ec497301ee6358
ShahSau <shahriarksaurov@gmail.com>
2021-07-01 12:30:23 +0300

    redux

--- froentend/src/App.js
+++ froentend/src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Container>
           <Route path='/' exact component={HomeScreen} />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
      
