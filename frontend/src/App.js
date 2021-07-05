9be038e4d112ddb21af438c7118acf821dd46e07
ShahSau <shahriarksaurov@gmail.com>
2021-07-05 12:44:15 +0300

    cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ProductScreen from './screens/ProductScreen';
-
+import CartScreen from './screens/CartScreen';
 
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
         <Container>
           <Route  path='/' exact component={HomeScreen}/>
           <Route  path='/product/:id'  component={ProductScreen}/>
+          <Route  path='/cart/:id?'  component={CartScreen}/>
 
         </Container>
       </main>
