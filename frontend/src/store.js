17354d7f8590be2f2ae4e1b457297a4da0b0c677
ShahSau <shahriarksaurov@gmail.com>
2021-07-07 18:43:18 +0300

    update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,20 +3,29 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducers,productDetailsReducer  } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 
 const reducer=combineReducers({
     productList: productListReducers,
     productDetails:productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 })
 
 const cartItemFromStorage = localStorage.getItem('cartItems') ?
     JSON.parse(localStorage.getItem('cartItems')) : []
 
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+    JSON.parse(localStorage.getItem('userInfo')) : null
+
+
 const initialState = {
-    cart: {cartItems: cartItemFromStorage}
+    cart: {
+        cartItems: cartItemFromStorage
+    },
+    userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
