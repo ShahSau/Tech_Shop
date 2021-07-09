87fa9f6cd1e4a2883a451a1a11069d5f08cf0498
ShahSau <shahriarksaurov@gmail.com>
2021-07-09 20:55:41 +0300

    user list

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -118,24 +118,24 @@ export const userUpdateProfileReducer = (state = {}, action) => {
 }
 
 
-// export const userListReducer = (state = { users: [] }, action) => {
-//     switch (action.type) {
-//         case USER_LIST_REQUEST:
-//             return { loading: true }
+export const userListReducer = (state = { users: [] }, action) => {
+    switch (action.type) {
+        case USER_LIST_REQUEST:
+            return { loading: true }
 
-//         case USER_LIST_SUCCESS:
-//             return { loading: false, users: action.payload }
+        case USER_LIST_SUCCESS:
+            return { loading: false, users: action.payload }
 
-//         case USER_LIST_FAIL:
-//             return { loading: false, error: action.payload }
+        case USER_LIST_FAIL:
+            return { loading: false, error: action.payload }
 
-//         case USER_LIST_RESET:
-//             return { users: [] }
+        case USER_LIST_RESET:
+            return { users: [] }
 
-//         default:
-//             return state
-//     }
-// }
+        default:
+            return state
+    }
+}
 
 
 // export const userDeleteReducer = (state = {}, action) => {
