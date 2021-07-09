8334d40c5a57a8f6622e6f3f4c7d5cda4e0c03d5
ShahSau <shahriarksaurov@gmail.com>
2021-07-10 02:57:18 +0300

    user functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 
 function App() {
@@ -33,7 +34,7 @@ function App() {
           <Route  path='/order/:id?'  component={OrderScreen}/>
 
           <Route  path='/admin/userlist'  component={UserListScreen}/>
-          
+          <Route  path='/admin/user/:id/edit'  component={UserEditScreen}/>
 
         </Container>
       </main>
