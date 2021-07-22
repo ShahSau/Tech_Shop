904fad82bd350e7a0a0e168f62fa1309487da2fb
ShahSau <shahriarksaurov@gmail.com>
2021-07-23 00:54:19 +0300

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,10 +124,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TechShop',
+        'USER': 'postgres',
+        'PASSWORD':'74247427sauROV',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
